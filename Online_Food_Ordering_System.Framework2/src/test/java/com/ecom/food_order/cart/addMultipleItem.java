69d98108c129d04d69ad586012daa15d60057590
Rahavi <rahavisubha00@gmail.com>
2025-05-20 13:27:51 +0530

    Pom Update

--- Online_Food_Ordering_System.Framework2/src/test/java/com/ecom/food_order/cart/addMultipleItem.java
+++ Online_Food_Ordering_System.Framework2/src/test/java/com/ecom/food_order/cart/addMultipleItem.java
@@ -1,6 +1,7 @@
 package com.ecom.food_order.cart;
 
 import org.testng.Assert;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 import com.ecom.food_order.baseclass.baseClass;
@@ -12,14 +13,20 @@ public class addMultipleItem extends baseClass{
 	public void add_MultipleItem() throws Throwable {
 		wLib.waitForPageLoad(driver);
 		Homepage hp = new Homepage(driver);
+		Reporter.log("Home page is displayed",true);
 		wLib.scrollByAmount(driver,hp.getGamberonidishLink());
 		hp.getLobsterdishLink().click();
+		Reporter.log("gamberoni dish is clicked",true);
 		NorthStreetpage np = new NorthStreetpage(driver);
+		Reporter.log("Navigated to north street restaurant page",true);
 		wLib.scrollByAmount(driver,np.getStuffedAddToCart());
 		String qty = elib.getdatafromexcel("food", 1, 1);
 		np.getQtyLobsterField().clear();
+		Reporter.log("lobster quantity field is cleared",true);
 		np.getQtyLobsterField().sendKeys(qty);
+		Reporter.log("qty is added",true);
 		wLib.scrollByAmount(driver,np.getLobsterAddToCart());
+		Reporter.log("dish added to cart",true);
 		np.getLobsterAddToCart().click();
 		wLib.scrollByAmount(driver, np.getStuffedAddToCart());
 	    String actQty=np.getActualquan().getText();
